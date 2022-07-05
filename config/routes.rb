b5d30b2bbe9c8dd811fcb9a9852e1a07db1a699c
Katja Demas Glaser <katjaglaeser@icloud.com>
2022-07-06 00:55:18 +0200

    navbar and view but not done

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :bookmarks, only: [:new, :create]
     resources :reviews, only: :create
   end
-  resources :bookmarks, only: :destroy
+  resources :bookmarks, only: [:destroy, :edit, :update]
   resources :reviews, only: :destroy
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
